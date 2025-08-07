a899d6afad82c5c3e54e08d10192ad31467af102
kho-mohamed <simopolo4@gmail.com>
2025-08-07 11:33:22 -0400

    configuration de CRUD

---
+++ controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Controllers;
+
+use App\Providers\View;
+use App\Providers\Validator;
+use App\Models\User;
+
+class AuthController{
+    
+    public function index(){
+        return View::render('auth/index');
+    }
+    public function store($data){
+        $validator = new Validator;
+        $validator->field('login', $data['login'])->min(2)->max(50)->email();
+        $validator->field('motDePasse', $data['motDePasse'])->min(6)->max(20);
+         if($validator->isSuccess()){
+            $user = new User;
+            $checkuser = $user->checkUser($data['login'], $data['motDePasse']);
+            if($checkuser){
+                return View::redirect('client/create'); // rediriger vers page catalogue quand elle sera disponible
+            }else{
+                $errors['message'] = "Les données saisies sont incorrectes!";
+                return View::render('auth/index', ['errors'=>$errors]);
+            }
+
+        }else{
+            $errors = $validator->getErrors();
+            return View::render('auth/index', ['errors'=>$errors]);
+        }
+
+
+    }
+    public function delete(){
+        session_destroy();
+        return View::redirect('login');
+    }
+}
+
+?>
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -7,3 +7,53 @@ use App\Providers\Auth;
 use App\Models\Privilege;
 use App\Models\User;
 
+class UserController
+{
+    public function __construct()
+    {
+        Auth::session();
+    }
+    public function create()
+    {
+
+        return View::render('user/create');
+    }
+
+    public function store($data)
+    {
+        $validator = new Validator();
+        $validator->field('nom', $data['nom'])
+            ->required()
+            ->min(3)
+            ->max(50);
+        $validator->field('prenom', $data['prenom'])
+            ->required()
+            ->min(3)
+            ->max(50);
+        $validator->field('login', $data['login'])
+            ->required()
+            ->min(3)
+            ->max(50)
+            ->unique('user');
+        $validator->field('motDePasse', $data['motDePasse'])
+            ->required()
+            ->min(6)
+            ->max(20);
+        $validator->field('email', $data['email'])
+            ->required()
+            ->email();
+
+        if ($validator->isSuccess()) {
+            $user = new User();
+            $data['motDePasse'] = $user->hashPassword($data['motDePasse']);
+            $insert = $user->insert($data);
+            return View::redirect('login');
+        } else {
+            $errors = $validator->getErrors();
+            return View::render('user/create', [
+                'errors' => $errors,
+                'user' => $data
+            ]);
+        }
+    }
+}
